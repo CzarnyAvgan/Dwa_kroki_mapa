3a89ed962ec2bb689d61ce36fef5511ef081a241
CzarnyAvgan <kacperkapiwysocki@gmail.com>
2016-04-25 20:23:42 +0200

    Try to do maps
    help

---
+++ app/src/main/java/pl/noskilljustfun/dwa_kroki_stad/MainActivity.java
@@ -0,0 +1,14 @@
+package pl.noskilljustfun.dwa_kroki_stad;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
